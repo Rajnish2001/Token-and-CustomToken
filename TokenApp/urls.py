48ff7fbcc5cce0d0a6b45f9fcfc192f7657c74cb
Rajnish Kumar <rsh.globalia@gmail.com>
2022-07-07 13:00:37 +0530

    How To Generate Token and CustomToken

---
+++ TokenApp/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path,include
+from .import views
+from rest_framework.routers import DefaultRouter
+# from rest_framework.authtoken.views import obtain_auth_token
+from .auth import CustomAuthToken
+
+router = DefaultRouter()
+router.register('studentapi/',views.StudentModelViewSet,basename='student')
+router.register('studentapi/<int:id>',views.StudentModelViewSet,basename='student')
+
+urlpatterns = [
+    path('',include(router.urls)),
+    path('cust/',include('rest_framework.urls')),
+    # path('gettoken/',obtain_auth_token),
+    path('gettoken/',CustomAuthToken.as_view()),
+]
